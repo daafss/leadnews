2f8c1d52e4c5025bcbbf641e31eb0c85a5f395d1
joker <joker@itcast.cn>
2024-09-01 15:23:15 +0800

    swagger相关配置

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -2,14 +2,14 @@ package com.heima.user;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Author: violet
  * Date: 2024/9/1 14:05
  */
-@SpringBootTest
+@SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.heima.user.mapper")
 public class UserApplication {
--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -3,6 +3,8 @@ package com.heima.user.controller.v1;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.user.dtos.LoginDto;
 import com.heima.user.service.ApUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1/login")
+@Api(value = "app端用户登陆", tags = "ap_user", description = "app端用户登陆API")
 public class ApUserLoginController {
 
     @Autowired
@@ -26,6 +29,7 @@ public class ApUserLoginController {
      * @return
      */
     @PostMapping("/login_auth")
+    @ApiOperation("用户登陆")
     public ResponseResult login(@RequestBody LoginDto dto){
         return apUserService.login(dto);
     }
